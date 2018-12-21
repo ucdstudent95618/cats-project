460ee8368ad24d977e65fc953858e0dd67ddc5fc
Miguel <migsalazar@ucdavis.edu>
2018-12-20 17:25:36 -0800

    Updates

---
+++ precache-manifest.d79080a9f519e7119f647d9bfc53de8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89664fead03366653f68",
+    "url": "/cats-project/static/js/runtime~main.89664fea.js"
+  },
+  {
+    "revision": "00247cc7634a4df0b8ce",
+    "url": "/cats-project/static/js/main.00247cc7.chunk.js"
+  },
+  {
+    "revision": "2060950a28bc6b1f5f03",
+    "url": "/cats-project/static/js/1.2060950a.chunk.js"
+  },
+  {
+    "revision": "00247cc7634a4df0b8ce",
+    "url": "/cats-project/static/css/main.1325c00c.chunk.css"
+  },
+  {
+    "revision": "6dadd9d9518c5837529a2f0844605c1a",
+    "url": "/cats-project/index.html"
+  }
+];
